5380d960259301e74f2fcea12782d634a471bfc0
changchanghwang <hch950627@naver.com>
2024-06-21 16:53:41 +0900

    study

---
+++ go_study_ecube/0620/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var a = 1
+
+func main() {
+	var b = 2
+	c := 3
+	const d = 4
+
+	if c == 3 {
+		e := 5
+		fmt.Println(a, b, c, d, e)
+	}
+
+	fmt.Println(a, b, c, d)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//  while loop
+	// for  a < 10 {
+	// 	fmt.Println(a)
+	// }
+
+	// for {
+	// 	fmt.Println("Infinite loop")
+	// }
+	test()
+}
+
+func test() {
+	defer fmt.Println("hello world")
+	defer fmt.Println("hello world2")
+
+	time.Now()
+	defer fmt.Println("hello world3")
+}
